8b7d02cd5b4bdfc51ab56c1f80d93a02653fbb85
dev <dev@dev.com>
2021-09-13 10:15:40 +0530

    commit

---
+++ welcome.java
@@ -0,0 +1,8 @@
+// RULES & CONVENTIONS
+ class welcome  {
+   public static void main(String args[]){
+      System.out.println("welcome");;;;;;;;;;;
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   }
+}
