ed6d9de154c1191b602a1915aba0aa4f4f2c257c
dev <dev@dev.com>
2021-09-13 11:32:41 +0530

    novice programming

---
+++ novice/Welcome.java
@@ -0,0 +1,16 @@
+// CAUTION : THIS IS ONLY TO KNOW THE WAYS NOT TO DO SOMETHING.
+// RULES & CONVENTIONS
+public class NoviceWelcome  {
+   public static void main(String args[]){
+      System.out.println("welgo");
+      add(1,2);
+   }
+   public static void add (int num1, int num2){
+      // very simple logic (super complex logic)
+      System.out.println(num1 + num2);
+   }
+}
+
+class Test {
+
+}
