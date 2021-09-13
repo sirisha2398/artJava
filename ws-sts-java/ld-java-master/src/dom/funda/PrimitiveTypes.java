a9822546b29afb9ad0d19a5db0a874a8fec617cb
Dhairya <dhparekh@paypal.com>
2021-09-13 14:18:36 +0530

    IP-1

--- ws-sts-java/ld-java-master/src/dom/funda/PrimitiveTypes.java
+++ ws-sts-java/ld-java-master/src/dom/funda/PrimitiveTypes.java
@@ -5,14 +5,23 @@ public class PrimitiveTypes {
 	public static void main(String[] args) {
 		PrivitiveDataTypes privitiveDataTypes = new PrivitiveDataTypes();
 		System.out.println(privitiveDataTypes.bVar);
+		System.out.println(privitiveDataTypes.intVar);
+		System.out.println(privitiveDataTypes.charVar);
+		System.out.println(privitiveDataTypes.floatVar);
+		System.out.println(privitiveDataTypes.doublVar);
+		System.out.println(privitiveDataTypes.shortVar);
+		System.out.println(privitiveDataTypes.longVar);
 		System.out.println(privitiveDataTypes.booleanVar);
 	}
 }
-
-// Class : Classification 
-// Class is a blueprint or specification for creating objects.
-// Many instance can be created for a class.
 class PrivitiveDataTypes {
 	byte bVar;
 	boolean booleanVar;
+	int intVar;
+	float floatVar;
+	double doublVar;
+	long longVar;
+	short shortVar;
+	char charVar;
+
 }
